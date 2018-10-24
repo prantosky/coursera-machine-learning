512dcb1450ca772a08f9553d4486a8a8472955af
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2018-10-24 12:43:46 +0530

    Add Week4_Assignment -> lrCostFunction.m

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,14 +36,10 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
-
-
-
-
+h_x=sigmoid(X * theta);
+theta_new = [0 ; theta(2:size(theta))]
+J = (1 / m) .* (-y' * log(h_x) - (1 - y)' * log(1 - h_x)) + (lambda/(2*m)) * (theta_new' * theta_new);
+grad = (1/m) * (X' * (h_x - y)) + (lambda/m) .* theta_new;
 
 % =============================================================
 
