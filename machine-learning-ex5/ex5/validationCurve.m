9d62a7fc62a91a360b58282e4802ec35b3ffd544
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2018-11-09 16:08:40 +0530

    Week6 Assignment -> Complete the validationCurve function

--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -39,13 +39,13 @@ error_val = zeros(length(lambda_vec), 1);
 %
 %
 
-
-
-
-
-
-
-
+for i = 1:length(lambda_vec)
+	lambda = lambda_vec(i);
+	[theta] = trainLinearReg(X, y, lambda);
+    [J_train ,_] = linearRegCostFunction(X, y, theta, 0);
+    [J_val ,_] = linearRegCostFunction(Xval, yval, theta, 0);
+    error_train(i) = J_train;
+    error_val(i) = J_val;
 
 
 % =========================================================================
